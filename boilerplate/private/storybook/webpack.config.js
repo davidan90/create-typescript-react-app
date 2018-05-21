4e556e2ddfcb9a4f1d631dc3f3e44b5f813221b3
davidan90 <david16an@gmail.com>
2018-05-21 12:43:12 +0200

    change storybook WP config

--- boilerplate/private/storybook/webpack.config.js
+++ boilerplate/private/storybook/webpack.config.js
@@ -3,7 +3,7 @@ const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader')
+    loader: require.resolve('ts-loader')
   });
   defaultConfig.plugins.push(new TSDocgenPlugin());
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
